f7a9bf6e90a236158d9ad5281ca356689f8c10cf
SrSadra <sadraspurs@gmail.com>
2022-02-25 09:47:01 +0330

    taghirat

--- kalim.java
+++ kalim.java
@@ -1,5 +1,7 @@
 public class kalim{
     public static void main(String[] args) {
         System.out.println("this is from branch shalghami");
+        System.out.println("taghir eyval");
+        System.out.println(4+6);
     }
 }
